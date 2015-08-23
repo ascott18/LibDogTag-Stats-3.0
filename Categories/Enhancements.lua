ff111a58000b9330ea72175d8e1cd17740eb3a50
cybeloras <ascott18@gmail.com>
2015-08-23 20:53:14 +0000

    Removed pre-6.0 compatibility, and added an event to the CriticalStrike tag.
    git-svn-id: https://repos.curseforge.com/wow/libdogtag-stats-3-0/trunk@28 c86bc0c8-76ea-4e27-b18a-c07b460437de

--- Categories/Enhancements.lua
+++ Categories/Enhancements.lua
@@ -8,7 +8,6 @@ end
 DogTag_Stats_funcs[#DogTag_Stats_funcs+1] = function(DogTag_Stats, DogTag)
 
 local L = DogTag_Stats.L
-local wow_600 = select(4, GetBuildInfo()) >= 60000
 
 
 DogTag:AddTag("Stats", "Mastery", {
@@ -32,61 +31,69 @@ DogTag:AddTag("Stats", "Spirit", {
 	category = L["Enhancements"],
 })
 
-if wow_600 then
-	DogTag:AddTag("Stats", "CriticalStrike", {
-		code = GetCritChance,
-		ret = "number",
-		events = "COMBAT_RATING_UPDATE",
-		doc = L["Returns your crit chance"],
-		example = '[CriticalStrike:Round(1)] => "23.4"; [CriticalStrike:Round(1):Percent] => "23.4%"',
-		category = L["Enhancements"],
-	})
 
-	DogTag:AddTag("Stats", "Haste", {
-		code = GetHaste,
-		ret = "number",
-		events = "UNIT_ATTACK_SPEED#player",
-		doc = L["Returns your haste percentage"],
-		example = '[Haste:Round(1)] => "32.7"; [Haste:Round(1):Percent] => "32.7%"',
-		category = L["Enhancements"],
-	})
+DogTag:AddTag("Stats", "CriticalStrike", {
+	code = GetCritChance,
+	ret = "number",
+	events = "PLAYER_DAMAGE_DONE_MODS;COMBAT_RATING_UPDATE",
+	doc = L["Returns your crit chance"],
+	example = '[CriticalStrike:Round(1)] => "23.4"; [CriticalStrike:Round(1):Percent] => "23.4%"',
+	category = L["Enhancements"],
+})
 
-	
-	DogTag:AddTag("Stats", "Multistrike", {
-		code = GetMultistrike,
-		ret = "number",
-		events = "MULTISTRIKE_UPDATE",
-		doc = L["Returns your Multistrike percentage"],
-		example = '[Multistrike:Round(1)] => "17.2"; [Multistrike:Round(1):Percent] => "17.2%"',
-		category = L["Enhancements"],
-	})
-	DogTag:AddTag("Stats", "Lifesteal", {
-		code = GetLifesteal,
-		ret = "number",
-		events = "LIFESTEAL_UPDATE",
-		doc = L["Returns your Lifesteal percentage"],
-		example = '[Lifesteal:Round(1)] => "1.2"; [Lifesteal:Round(1):Percent] => "1.2%"',
-		category = L["Enhancements"],
-	})
-	DogTag:AddTag("Stats", "Versatility", {
-		code = function()
-			return GetCombatRatingBonus(CR_VERSATILITY_DAMAGE_DONE) + GetVersatilityBonus(CR_VERSATILITY_DAMAGE_DONE)
-		end,
-		ret = "number",
-		events = "COMBAT_RATING_UPDATE",
-		doc = L["Returns your Versatility damage and healing increase percentage."],
-		example = '[Versatility] => "3.6"; [Versatility:Round(1):Percent] => "3.6%"',
-		category = L["Enhancements"],
-	})
-	DogTag:AddTag("Stats", "Avoidance", {
-		code = GetAvoidance,
-		ret = "number",
-		events = "AVOIDANCE_UPDATE",
-		doc = L["Returns your Avoidance percentage"],
-		example = '[Avoidance:Round(1)] => "2.2"; [Avoidance:Round(1):Percent] => "2.2%"',
-		category = L["Enhancements"],
-	})
-end
+DogTag:AddTag("Stats", "Haste", {
+	code = GetHaste,
+	ret = "number",
+	events = "UNIT_ATTACK_SPEED#player",
+	doc = L["Returns your haste percentage"],
+	example = '[Haste:Round(1)] => "32.7"; [Haste:Round(1):Percent] => "32.7%"',
+	category = L["Enhancements"],
+})
+
+
+DogTag:AddTag("Stats", "Multistrike", {
+	code = GetMultistrike,
+	ret = "number",
+	events = "MULTISTRIKE_UPDATE",
+	doc = L["Returns your Multistrike percentage"],
+	example = '[Multistrike:Round(1)] => "17.2"; [Multistrike:Round(1):Percent] => "17.2%"',
+	category = L["Enhancements"],
+})
+
+DogTag:AddTag("Stats", "Leech", {
+	code = GetLifesteal,
+	ret = "number",
+	events = "LIFESTEAL_UPDATE",
+	doc = L["Returns your Leech percentage"],
+	example = '[Leech:Round(1)] => "1.2"; [Leech:Round(1):Percent] => "1.2%"',
+	category = L["Enhancements"],
+})
+
+-- The leech tag was originally called Lifesteel. Make an alias so it still works for people who were using it.
+DogTag:AddTag("Stats", "Lifesteal", {
+	alias = "[Leech]",
+	noDoc = true,
+})
+
+DogTag:AddTag("Stats", "Versatility", {
+	code = function()
+		return GetCombatRatingBonus(CR_VERSATILITY_DAMAGE_DONE) + GetVersatilityBonus(CR_VERSATILITY_DAMAGE_DONE)
+	end,
+	ret = "number",
+	events = "COMBAT_RATING_UPDATE",
+	doc = L["Returns your Versatility damage and healing increase percentage."],
+	example = '[Versatility] => "3.6"; [Versatility:Round(1):Percent] => "3.6%"',
+	category = L["Enhancements"],
+})
+
+DogTag:AddTag("Stats", "Avoidance", {
+	code = GetAvoidance,
+	ret = "number",
+	events = "AVOIDANCE_UPDATE",
+	doc = L["Returns your Avoidance percentage"],
+	example = '[Avoidance:Round(1)] => "2.2"; [Avoidance:Round(1):Percent] => "2.2%"',
+	category = L["Enhancements"],
+})
 
 
 end
